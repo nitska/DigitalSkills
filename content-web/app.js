12699889d5e3068ef3717f9711ada8c382ffecb4
nitska <91181392+nitska@users.noreply.github.com>
2021-09-22 16:12:47 +0300

    Update app.js

--- content-web/app.js
+++ content-web/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const request = require('request');
-
+//and
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
